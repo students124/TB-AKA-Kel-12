5b665eb16480de1786a2aee5d4060353463ac5ce
students124 <billyyuriaan@students.telkomuniversity.ac.id>
2021-12-18 19:51:41 +0700

    added more to the list

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
 # importing the required module
+import timeit
+import numpy as np
 from src.BubbleSort import bubbleSort
 from src.RadixSort import countingSort, radixSort
 from src.ufr import printArray,inputArray
 from src.plot import makePlot
 
 def main():
+    # arr10 = random.sample(range(1,1000), 10)
+    arr10 = list(np.random.randint(low=1, high=1000, size=10))
+    arr100 = list(np.random.randint(low=1, high=1000, size=100))
+    arr1000 = list(np.random.randint(low=1, high=1000, size=1000))
+    arr10000 = list(np.random.randint(low=1, high=1000, size=10000))
+    arr100000 = list(np.random.randint(low=1, high=1000, size=100000))    
+
+    #n = 10 for radix
+    rn10 = timeit.timeit(lambda : radixSort(arr10))
+    #n = 100 for radix
+    rn100 = timeit.timeit(lambda : radixSort(arr100))
+    #n = 1000 for radix
+    rn1000 = timeit.timeit(lambda : radixSort(arr1000))
+    #n = 10000 for radix
+    rn10000 = timeit.timeit(lambda : radixSort(arr10000))
+    #n = 1000 for radix
+    rn100000 = timeit.timeit(lambda : radixSort(arr100000))
+
+    #n = 10 for bubble
+    dn10 = timeit.timeit(lambda : bubbleSort(arr10))
+    #n = 100 for bubble
+    dn100 = timeit.timeit(lambda : bubbleSort(arr100))
+    #n = 1000 for bubble
+    dn1000 = timeit.timeit(lambda : bubbleSort(arr1000))
+    #n = 10000 for bubble
+    dn10000 = timeit.timeit(lambda : radixSort(arr10000))
+    #n = 100000 for bubble
+    dn100000 = timeit.timeit(lambda : radixSort(arr100000))
+
+
     # x axis values for radix
-    x1 = [1,2,3]
+    x1 = [rn10, rn100, rn1000,rn10000,rn100000]
     # corresponding y axis values for radix
-    y1 = [2,4,1]
+    y1 = [10,100,1000,10000,100000]
 
     # x axis values for radix for bubble
-    x2 = [4,5,6]
+    x2 = [dn10,dn100,dn1000,dn10000,dn100000]
     # corresponding y axis values for bubble
-    y2 = [4,8,2]
+    y2 = [10,100,1000,10000,100000]
 
-    makePlot(x1,y1,x2,y2)
+    makePlot(x1,y1,x2,y2, "Running Time", "Input Size", "Radix VS Bubble")
     
 
 if __name__ == '__main__':
--- src/plot.py
+++ src/plot.py
@@ -1,17 +1,25 @@
 import matplotlib.pyplot as plt
 
-def makePlot(x1 : int,y1 : int,x2 : int,y2 : int):
+def makePlot(x1 : int,y1 : int,x2 : int,y2 : int, xLabel : str, yLabel : str, title : str):
     # plotting the points
-    plt.plot(x1, y1)
-    plt.plot(x2,y2)
+    plt.plot(x1, y1, color='r', label='Radix')
+    
+    plt.plot(x2,y2, color='g', label='Bubble')
+    
     
     # naming the x axis
-    plt.xlabel('Input Size')
+    plt.xlabel(xLabel)
     # naming the y axis
-    plt.ylabel('Running time')
+    plt.ylabel(yLabel)
     
     # giving a title to my graph
-    plt.title('Radix VS Bubble')
+    plt.title(title)
     
+    # giving a grid
+    plt.grid()
+
+    # showing all the legend mark
+    plt.legend()
+
     # function to show the plot
     plt.show()

d301c18f931ca3dadb1e7bc963894e4061d2b226
students124 <billyyuriaan@students.telkomuniversity.ac.id>
2021-12-30 17:08:08 +0700

    update

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import pandas as pd
 from src.BubbleSort import bubbleSort
 from src.RadixSort import countingSort, radixSort
 from src.ufr import printArray,inputArray
-# from src.plot import makePlot
+from src.plot import makePlot
 
 
 def main():
-    # arr10 = random.sample(range(1,1000), 10)
     arr10 = list(np.random.randint(low=1, high=1000, size=10))
     arr100 = list(np.random.randint(low=1, high=1000, size=100))
     arr1000 = list(np.random.randint(low=1, high=1000, size=1000))
@@ -28,7 +27,7 @@ def main():
     #n = 10000 for radix
     rn10000 = timeit.timeit(lambda : radixSort(arr10000))
 
-    #n = 1000 for radix
+    # #n = 1000 for radix
     rn100000 = timeit.timeit(lambda : radixSort(arr100000))
 
     #n = 10 for bubble
@@ -40,30 +39,32 @@ def main():
     # #n = 1000 for bubble
     dn1000 = timeit.timeit(lambda : bubbleSort(arr1000))
 
-    # #n = 10000 for bubble
+    # # #n = 10000 for bubble
     dn10000 = timeit.timeit(lambda : radixSort(arr10000))
 
-    # #n = 100000 for bubble
+    # # #n = 100000 for bubble
     dn100000 = timeit.timeit(lambda : radixSort(arr100000))
 
 
-    # x axis values for radix
-    x1 = [rn10, rn100, rn1000,rn10000,rn100000]
     # corresponding y axis values for radix
-    y1 = [10,100,1000,10000,100000]
+    x1 = [10,100,1000,10000,100000]
+    # x axis values for radix
+    y1 = [rn10,rn100,rn1000,rn10000,rn10000]
 
-    # x axis values for radix for bubble
-    x2 = [dn10,dn100,dn1000,dn10000,dn100000]
     # corresponding y axis values for bubble
-    # y2 = [10,100,1000,10000,100000]
+    x2 = [10,100,1000,10000,100000]
+    # x axis values for radix for bubble
+    y2 = [rn10,rn100,rn1000,rn10000,rn100000]
 
     df = pd.DataFrame({
-        "Data" : y1,
-        "Radix" : x1,
-        "Bubble" : x2
+        "Data" : x1,
+        "Radix" : y1,
+        "Bubble" : y2
     })
 
     print(df)
+    makePlot(x1,y1,x2,y2,"Data n", "Running Time", "Radix VS Bubble")
+
     
 
 if __name__ == '__main__':
